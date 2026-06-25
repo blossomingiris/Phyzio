fb285090b7400eea95ef18dd2864172a835aa099
Ksenia Rachouti <blossomingiris@gmail.com>
2026-06-25 14:07:14 +0300

    feat: update project folder structure, add .claudeignore

--- app/config/db.ts
+++
@@ -1,1 +0,0 @@
-export const APP_DATABASE_URL: string = process.env.DATABASE_URL!;
--- app/dto/shared.ts
+++
@@ -1,7 +0,0 @@
-import { timestamp } from "drizzle-orm/pg-core";
-import Type from "typebox";
-
-export const Timestamp = Type.Object({
-  createdAt: timestamp("created_at").defaultNow(),
-  updatedAt: timestamp("updated_at").defaultNow(),
-});
